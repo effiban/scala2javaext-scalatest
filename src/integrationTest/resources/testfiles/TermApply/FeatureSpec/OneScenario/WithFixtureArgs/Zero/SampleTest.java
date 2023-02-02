593af8f36e67d5abff39713009e338c5e2392758
Effi Ban <effi.ban@kenshoo.com>
2023-02-02 22:55:58 +0200

    Partial support for registration with fixture args

---
+++ src/integrationTest/resources/testfiles/TermApply/FeatureSpec/OneScenario/WithFixtureArgs/Zero/SampleTest.java
@@ -0,0 +1,30 @@
+package dummy;
+
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions.*;
+
+public class SampleTest {
+
+    public SampleTest() {
+    }
+
+    @Nested
+    @DisplayName("air conditioner")
+    public class AirConditioner {
+
+        public AirConditioner() {
+        }
+
+        @Test
+        @DisplayName("cooling")
+        public void cooling() {
+            ac.checkCooling();
+        }
+    }
+}
