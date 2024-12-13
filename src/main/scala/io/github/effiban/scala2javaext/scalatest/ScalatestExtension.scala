352c37fa73e44486210e8df5666fd4f5b144aa0a
effiban <effi.ban@skai.io>
2024-12-13 20:52:56 +0200

    Upgrade scala2java-spi to 10.2.1

--- src/main/scala/io/github/effiban/scala2javaext/scalatest/ScalatestExtension.scala
+++ src/main/scala/io/github/effiban/scala2javaext/scalatest/ScalatestExtension.scala
@@ -1,13 +1,11 @@
 package io.github.effiban.scala2javaext.scalatest
 
 import io.github.effiban.scala2java.spi.Scala2JavaExtension
-import io.github.effiban.scala2javaext.scalatest.predicates.ScalatestPredicates
 import io.github.effiban.scala2javaext.scalatest.transformers.ScalatestTransformers
 
 import scala.meta.{Term, XtensionQuasiquoteTerm}
 
 class ScalatestExtension extends Scala2JavaExtension
-  with ScalatestPredicates
   with ScalatestTransformers {
 
   override def shouldBeAppliedIfContains(termSelect: Term.Select): Boolean =
--- src/main/scala/io/github/effiban/scala2javaext/scalatest/predicates/ScalatestPredicates.scala
+++
@@ -1,8 +0,0 @@
-package io.github.effiban.scala2javaext.scalatest.predicates
-
-import io.github.effiban.scala2java.spi.predicates.{ExtendedPredicates, TemplateInitExcludedPredicate}
-
-trait ScalatestPredicates extends ExtendedPredicates {
-
-  override def templateInitExcludedPredicate(): TemplateInitExcludedPredicate = ScalatestTemplateInitExcludedPredicate
-}
